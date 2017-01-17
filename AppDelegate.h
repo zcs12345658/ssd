b15102c44dc226d3d05bbce7da1065c1f4cf388a
心中无码 <apple@cmwap.cqmc.com>
2017-01-17 15:44:30 +0800

    显得工程

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ssd
+//
+//  Created by apple on 2017/1/17.
+//  Copyright © 2017年 jinke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ssd
+//
+//  Created by apple on 2017/1/17.
+//  Copyright © 2017年 jinke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
